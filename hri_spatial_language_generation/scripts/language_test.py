de56f43157fa8d6684932f9f4c75c6172447cb18
zhiyuhuo <huozhiyunewneu@gmail.com>
2016-01-04 15:04:47 -0600

    a little change in language

--- hri_spatial_language_generation/scripts/language_test.py
+++ hri_spatial_language_generation/scripts/language_test.py
@@ -4,34 +4,60 @@ import rospy
 from std_msgs.msg import String
 import re
 import numpy as np
-
-def load_rdt_relation_prob_matrix():
+  
+def load_rdt_relation_prob_dic():
     matfilname = '/home/hri/hri_DATA/spatial_language_generation/rdtkeyrelationprob.txt'
     with open(matfilname, 'r') as myfile:
         data = myfile.readlines()
-    return data
-  
-def generate_dic_from_rdt_prob_mat(mat):
     dic = {}
-    for m in mat:
-        line = m
+    for line in data:
         keyposition = re.search(r'^(.*):', line).span()
         datastr = line[keyposition[1]+1:]
         values = re.split(r'\s+', datastr)
-        key = line[0:keyposition[1]-1];
+        keyword = line[0:keyposition[1]-1];
         Lprob = len(values) - 1
         values = values[0: Lprob]
 	probs = [float(f) for f in values]
-	#print key
+	#print keyword
 	#print probs
-	dic[key] = probs
+	dic[keyword] = probs
     return dic
-
+  
+def load_rdt_content_dic():
+    dicfilename = '/home/hri/hri_DATA/spatial_language_generation/rdtcontent.txt'
+    with open(dicfilename, 'r') as myfile:
+        data = myfile.readlines()
+    dic = {}
+    for line in data:
+        keyposition = re.search(r'^(.*):', line).span()
+        datastr = line[keyposition[1]+1:]
+        contents = re.split(r',+', datastr)
+        keyword = line[0:keyposition[1]-1];
+        Lprob = len(contents) - 1
+        contents = contents[0: Lprob]
+        #print keyword
+        #print contents[0]
+        dic[keyword] = contents[0]
+    return dic
         
+def compute_structure_energy(grounding, rdtrelationprobdic):
+    keywordset = [];
+    for i in range(0, len(grounding)-1):
+        for j in range(0, len(grounding)-1):
+            if i != j:
+                keywordset.append(grounding[i] + '-' + grounding[j])
+    #print keywordset
+    for k in keywordset:
+        probs = rdtrelationprobdic[k]
+        print probs
+        print probs.index(max(probs))
+    
         
 if __name__ == '__main__':
-    grounding = ['bedroom', 'mug', 'room_right_non', 'chair_beside_table']
-    data = load_rdt_relation_prob_matrix()
-    dic = generate_dic_from_rdt_prob_mat(data)
+    grounding = ['bedroom', 'mug', 'room_right_non', 'chair_beside_non' 'non_non_table']
+    relationtypes = ['parent_left', 'parent_right', 'child_left', 'child_right', 'sibling_left', 'sibling_right']
+    rdtrelationprobdic = load_rdt_relation_prob_dic()
+    rdtcontentlist = load_rdt_content_dic()
+    compute_structure_energy(grounding, rdtrelationprobdic)
     print 'program finished.'
     
