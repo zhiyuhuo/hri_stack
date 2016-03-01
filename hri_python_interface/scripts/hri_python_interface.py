aa9e51df5ae455cb55f2f9cd3b424431c407a220
zhiyuhuo <huozhiyunewneu@gmail.com>
2016-03-01 13:15:28 -0600

    working on twitter python for robot

--- hri_python_interface/scripts/hri_python_interface.py
+++ hri_python_interface/scripts/hri_python_interface.py
@@ -1,45 +1,20 @@
 #!/usr/bin/env python
-#import roslib;roslib.load_manifest('hri_python_interface_client')
-#import re
-#import nltk
-#import nltk.chunk
-#import numpy
-#import random
-#import nltk.tag
-#import sys
-##from nltk.tag import brill_modified
-#from nltk.tag import brill
-##from nltk.etree.ElementTree import ElementTree
-#import xml.etree
-#from nltk import Tree
-#import rospy
-#from train_parser import *
-#from std_msgs.msg import String
-
-#tree = []
-#ifreceive = 0;
-#xmlfile = "/home/hri/hri_DATA/template.xml";
-
-#def callback(data):
-    #global ifreceive;
-    #global xmlfile;
-    #if ifreceive == 0:
-	#print data.data
-	#xmlfile = "/home/hri/hri_DATA/template.xml"
-	#tree = raw_to_xml(data.data, xmlfile)
-	#print xmlfile
-	#print "end chunking ~~~~~~~~~~~"
-	#ifreceive = 1;
-    #else:
-	#pub = rospy.Publisher('/address', String, queue_size=10)
-	#print "xml file ready ~~~~~~~~~~~~~~~~~~"
-	#pub.publish(xmlfile)
+import roslib;roslib.load_manifest('hri_python_interface')
+import rospy
+import sys
+from std_msgs.msg import String
+from twitter import *
 	
 
-#if __name__ == "__main__":
-    #rospy.init_node('part_of_speech')
-    #print "start part_of_speech node"
-    #rospy.Subscriber("/recognizer/output", String, callback)
-    #ifreceive = 0;
-    #rospy.spin();
+if __name__ == "__main__":
+    rospy.init_node('hri_python_interface_client')
+    print "start hri_python_interface_client node"
+
+    t = Twitter(
+    auth=OAuth(token, token_key, con_secret, con_secret_key))
+
+    pub = rospy.Publisher('/recognizer/output', String, queue_size=10)
+    msg = "the message"
+    pub.publish(msg)
+    rospy.spin();
       
