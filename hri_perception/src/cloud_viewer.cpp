0ac279c0c843f6a20395cbfe0c9407261363f1d1
zhiyuhuo <huozhiyunewneu@gmail.com>
2016-04-21 09:41:09 -0500

    make some change on cloud_viewer

--- hri_perception/src/cloud_viewer.cpp
+++ hri_perception/src/cloud_viewer.cpp
@@ -8,16 +8,36 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/parse.h>
     
+using namespace std;
     
 int 
-main ()
+main (int argc, char** argv)
 {
+  string filename;
+  if (argc > 1)
+  {
+    string tstr(argv[1]);
+    filename = tstr;
+  }
+  
+  if (filename.size()<1)
+  {
+    return -1;
+  }
+  else
+  {
+    if (filename[0] != '\\')
+    {
+      filename = "/home/hri/" + filename;
+    }
+  }
+  
   //get data
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::io::loadPCDFile ("/home/hri/pcd_chair.pcd", *cloud);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::io::loadPCDFile ("/home/hri/hri_DATA/test/scene.pcd", *cloud2);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ> ());
+  pcl::io::loadPCDFile (filename.c_str(), *cloud);
+//   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZ>);
+//   pcl::io::loadPCDFile ("/home/hri/hri_DATA/test/scene.pcd", *cloud2);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB> ());
     
   //get normal
   pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
@@ -29,21 +49,35 @@ main ()
   ne.compute (*normals);
   
   // Create the filtering object
-  pcl::VoxelGrid<pcl::PointXYZ> sor;
-  sor.setInputCloud (cloud2);
+  pcl::VoxelGrid<pcl::PointXYZRGB> sor;
+  sor.setInputCloud (cloud);
   sor.setLeafSize (0.05f, 0.05f, 0.05f);
   sor.filter (*cloud_filtered);
 
   //visualize
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
   viewer->setBackgroundColor (0, 0, 0);
-  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
-  viewer->addPointCloud<pcl::PointXYZ> (cloud_filtered, "sample cloud");
+  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud_filtered);
+  viewer->addPointCloud<pcl::PointXYZRGB> (cloud_filtered, "sample cloud");
   //viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
   //viewer->addPointCloudNormals<pcl::PointXYZRGB, pcl::Normal> (cloud, normals, 10, 0.05, "normals");
   //viewer->addCoordinateSystem (1.0);
   viewer->initCameraParameters ();
+  
+  std::vector<pcl::visualization::Camera> cam; 
+
+  //Save the position of the camera           
+  
+  //Print recorded points on the screen: 
+  viewer->getCameras(cam); 
+  
+cout << "Cam: " << endl 
+             << " - pos: (" << cam[0].pos[0] << ", "    << cam[0].pos[1] << ", "    << cam[0].pos[2] << ")" << endl 
+             << " - view: ("    << cam[0].view[0] << ", "   << cam[0].view[1] << ", "   << cam[0].view[2] << ")"    << endl 
+             << " - focal: ("   << cam[0].focal[0] << ", "  << cam[0].focal[1] << ", "  << cam[0].focal[2] << ")"   << endl;
+  
+
 
   while (!viewer->wasStopped ())
   {
